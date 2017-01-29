598d70df2338eccf8249c8eb9909aac35e716004
Tommy <twcarrington@gmail.com>
2017-01-29 14:11:12 -0500

    removed config file...contents moved into index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import App from './App';
 import './index.css';
 import * as firebase from 'firebase';
 
+var config = {
+  apiKey: "AIzaSyBnaFmkAfls8iMSO7cu7neGA2-tumO9g7M",
+  authDomain: "meal-shopping-planner.firebaseapp.com",
+  databaseURL: "https://meal-shopping-planner.firebaseio.com",
+  storageBucket: "meal-shopping-planner.appspot.com",
+  messagingSenderId: "155689214491"
+};
+
+firebase.initializeApp(config);
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
