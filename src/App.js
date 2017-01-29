3eae7854b084f178bafa57017e30baef47706a88
Tommy <twcarrington@gmail.com>
2017-01-29 15:21:43 -0500

    removed config file again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {data} from './config.js';
-
-
-
+import firebase from 'firebase';
 
 class App extends Component {
 
@@ -16,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const fooRef = data.ref().child('foo');
+    const fooRef = firebase.database().ref().child('foo');
 
     fooRef.on('value', snap => {
       this.setState( {
@@ -32,44 +29,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>{this.state.foo}</h2>
         </div>
-        <div>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </div>
       </div>
     );
   }
 }
 
 export default App;
-
-///////////
-//
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       blargh: 'twelve'
-//     };
-//   }
-//
-//   componentDidMount() {
-//     const practiceRootRef = internsPracticeDB.ref().child('producers/');
-//     practiceRootRef.on('value', snap => {
-//       practiceRootRef.innerText = JSON.stringify(snap.val(), null, 3);
-//       this.setState( {
-//         producers: practiceRootRef.innerText
-//       })
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.state.producers}</h1>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-//
