2457c1ec0e963a40004448603e55be78bed4b44d
Tommy <twcarrington@gmail.com>
2017-01-29 15:14:04 -0500

    was missing a closing parentheses...not sure if lack of config file was a problem..will try that out next

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import * as firebase from 'firebase';
+import {data} from './config.js';
+
 
 
 
@@ -10,18 +11,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      foo: 10
+      foo: "notBar"
     };
   }
 
   componentDidMount() {
-    const fooRef = firebase.database().ref().child('foo');
-    //
-    // fooRef.on('value', snap => {
-    //   this.setState( {
-    //     foo: snap.val()
-    //   })
-    // }
+    const fooRef = data.ref().child();
+
+    fooRef.on('value', snap => {
+      this.setState( {
+        foo: snap.val()
+      })
+    })
   }
 
   render() {
@@ -40,3 +41,35 @@ class App extends Component {
 }
 
 export default App;
+
+///////////
+//
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       blargh: 'twelve'
+//     };
+//   }
+//
+//   componentDidMount() {
+//     const practiceRootRef = internsPracticeDB.ref().child('producers/');
+//     practiceRootRef.on('value', snap => {
+//       practiceRootRef.innerText = JSON.stringify(snap.val(), null, 3);
+//       this.setState( {
+//         producers: practiceRootRef.innerText
+//       })
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.producers}</h1>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
+//
