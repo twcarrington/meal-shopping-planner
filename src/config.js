c202951f17a08ae33ab5d3deb0936287fa569f23
Tommy <twcarrington@gmail.com>
2017-01-29 14:04:32 -0500

    initial setup

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBnaFmkAfls8iMSO7cu7neGA2-tumO9g7M",
+  authDomain: "meal-shopping-planner.firebaseapp.com",
+  databaseURL: "https://meal-shopping-planner.firebaseio.com",
+  storageBucket: "meal-shopping-planner.appspot.com",
+  messagingSenderId: "155689214491"
+};
+
+let configApp = firebase.initializeApp(config);
+
+export const Data = configApp.data();
