2a3e364be474dc9f725125013b004848c96c6835
Tommy <twcarrington@gmail.com>
2017-01-31 15:48:58 -0500

    time to try iterating through data structure using forEach()

--- src/App.js
+++ src/App.js
@@ -8,21 +8,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      foo: "notBar",
-      inner: "inner",
       food: null
     };
   }
 
   componentDidMount() {
     const foodRef = firebase.database().ref('/food/');
-
     foodRef.on('value', snap => {
-      foodRef.innerText = JSON.stringify(snap.val(), null, 3);
       let foodRefString = JSON.stringify(snap.val(), null, 3);
       this.setState( {
-        foo: snap.val(),
-        inner: foodRef.innerText,
         food: foodRefString
       })
     });
@@ -32,9 +26,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div>
-          <h2>{this.state.inner}</h2>
           <h2>{this.state.food}</h2>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
